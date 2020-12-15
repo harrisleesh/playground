73cd227e80da422041f1b22d9fc04ed7cc4add33
이성훈 <harrisleesh@gmail.com>
2020-12-16 07:09:03 +0900

    Spring Bean 등록, 의존성 주입 - 어노테이션 - 설정 파일

--- spring-basic/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
+++ spring-basic/hello-spring/hello-spring/src/main/java/hello/hellospring/controller/HomeController.java
@@ -1,5 +1,6 @@
 package hello.hellospring.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,14 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-@RestController
-@RequestMapping("/api/v1/coffees")
+@Controller
 public class HomeController {
 
     @GetMapping
-    public String getCoffee(HttpSession session, HttpServletRequest httpServletRequest){
-
-        System.out.println(session.getId());
-        return "coffee";
+    public String home(){
+        return "home";
     }
 }
