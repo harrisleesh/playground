b460ecd0bdcbc4c3cb7a4d4de158fae0fd7b9445
이성훈 <harrisleesh@gmail.com>
2020-12-27 18:09:57 +0900

    의존관계 자동주입 - 조회한 빈을 모두 받을 때, List, Map - 자동, 수동의 기준 -- 기본적으로 자동주입을 사용하자 -- 설정정보나 드러내야 할 필요가 있는 경우 수동주입!

---
+++ core/src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,17 @@
+package hello.core.order;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder(){
+        //수정자로 autowired하면 생기는 문제!
+//        OrderServiceImpl orderService = new OrderServiceImpl();
+//        orderService.createOrder(1L, "itemA", 10000);
+
+    }
+
+}
