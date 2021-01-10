f51d059da1aa3d1086313868f713869597102244
이성훈 <harrisleesh@gmail.com>
2021-01-10 15:35:20 +0900

    Rainist - Test Problem

---
+++ 알고리즘/src/main/java/rainist/Test.java
@@ -0,0 +1,12 @@
+package rainist;
+
+public class Test {
+    public static void main(String[] args) {
+        int findValue = 5;
+        int[] arr = {1, 3, 3, 5, 8}; //정렬이 되어있다는 전제
+
+//        binary search tree
+
+    }
+
+}
