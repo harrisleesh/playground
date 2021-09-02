ba6315ce7860a88401d712247cfd64e67f8998dd
harris <harrisleesh@gmail.com>
2021-09-02 13:48:56 +0900

    feat : Solve TwoSum problems more fast

--- coding-test/src/main/java/leetcode/TwoSum.java
+++ coding-test/src/main/java/leetcode/TwoSum.java
@@ -17,5 +17,19 @@ public class TwoSum {
             }
             return null;
         }
+
+        public int[] twoSum2(int[] nums, int target) {
+            int[] result = new int[2];
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                if(map.containsKey(target - nums[i])){
+                    result[1] = i;
+                    result[0] = map.get(target - nums[i]);
+                    return result;
+                }
+                map.put(nums[i], i);
+            }
+            return result;
+        }
     }
 }
