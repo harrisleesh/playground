751b7fc8f97cebd6f747916c9a866866576f0ce7
harris <harrisleesh@gmail.com>
2021-09-08 20:30:26 +0900

    feat : Test lazy and async

--- javascript/lib/fx.js
+++ javascript/lib/fx.js
@@ -30,4 +30,29 @@ const reduce = curry((f, acc, iter) => {
 const go = (...args) => reduce((a, f) => f(a), args);
 const pipe = (f, ...fs) => (...as) => go(f(...as), ...fs);
 const add = (a, b) => a + b;
+
+const take = curry((l, iter) => {
+    let res = [];
+    for (const a of iter) {
+        res.push(a);
+        if (res.length == l) return res;
+    }
+    return res;
+})
+
+const L = [];
+L.range = function* gen(l) {
+    let i = -1;
+    while (++i < l) {
+        yield i;
+    }
+}
+
+L.map = curry(function* (f, iter) {
+    for (const el of iter) yield f(el);
+})
+
+L.filter = curry(function* (f, iter) {
+    for (const el of iter) if (f(el)) yield el;
+})
 log = console.log;
