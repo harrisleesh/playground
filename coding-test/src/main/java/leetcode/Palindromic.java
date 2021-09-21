5ce97be4943433a4b957903cd9911c950fcbf65e
harris <harrisleesh@gmail.com>
2021-09-21 20:40:12 +0900

    feat : Palindromic first commit

---
+++ coding-test/src/main/java/leetcode/Palindromic.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+public class Palindromic {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.longestPalindrome("abacde"));
+    }
+
+    static class Solution {
+        public String longestPalindrome(String s) {
+            return "";
+        }
+    }
+}
