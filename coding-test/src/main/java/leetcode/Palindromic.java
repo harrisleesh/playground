30c0d43ebe0836121373653735e18b7e8f2346cf
harris <harrisleesh@gmail.com>
2021-09-22 18:57:50 +0900

    feat : Palindromic complete

--- coding-test/src/main/java/leetcode/Palindromic.java
+++ coding-test/src/main/java/leetcode/Palindromic.java
@@ -4,12 +4,39 @@ public class Palindromic {
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.longestPalindrome("abacde"));
+        System.out.println(solution.longestPalindrome("babad"));
+        System.out.println(solution.longestPalindrome("cbbd"));
+        System.out.println(solution.longestPalindrome("a"));
+        System.out.println(solution.longestPalindrome("ac"));
     }
 
     static class Solution {
         public String longestPalindrome(String s) {
+            // 1, 3, 5, 7 순서로 스트링을 순회한다.
+            int length = s.length();
+            while (length > 0) {
+                for (int i = 0; i + length <= s.length(); i++) {
+                    if (checkPalindrome(s.substring(i, length+i))) {
+                        return s.substring(i, length + i);
+                    }
+
+                }
+                length--;
+            }
             return "";
+        }
+
+        private boolean checkPalindrome(String substring) {
+            if (substring.length() == 1) {
+                return true;
+            }
+            int halfLength = substring.length() / 2;
+            for (int i = 0; i < halfLength; i++) {
+                if (substring.charAt(i) != substring.charAt(substring.length() - i - 1)) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
