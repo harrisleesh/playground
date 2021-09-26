dd6b046effb1fd0b47d6c81d6d42c704f2675b64
harrisleesh <harrisleesh@gmail.com>
2021-09-27 07:45:19 +0900

    feat : Solve algorithm problems - Atoi

--- coding-test/src/main/java/leetcode/Atoi.java
+++ coding-test/src/main/java/leetcode/Atoi.java
@@ -5,4 +5,33 @@ public class Atoi {
     String to integer
     https://leetcode.com/problems/string-to-integer-atoi/
      */
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.myAtoi("42"));
+
+    }
+
+    static class Solution {
+        public int myAtoi(String s) {
+            String trimmedString = s.trim();
+            boolean minus = false;
+            String subString = trimmedString;
+            if (trimmedString.charAt(0) == '-') {
+                minus = true;
+                subString = trimmedString.substring(1);
+            } else if (trimmedString.charAt(0) == '+') {
+                subString = trimmedString.substring(1);
+            }
+
+            for (int i = 0; i < subString.length(); i++) {
+                char c = subString.charAt(i);
+                if ('0' > c  || c > '9') {
+                    System.out.println("not a number");
+
+                }
+            }
+            return 0;
+        }
+    }
 }
