a29f8bb4abfa897625901b93096ba0b25b8d8c86
harrisleesh <harrisleesh@gmail.com>
2021-09-28 07:43:05 +0900

    feat : Solve algorithm problems - Atoi - exception handling

--- coding-test/src/main/java/leetcode/Atoi.java
+++ coding-test/src/main/java/leetcode/Atoi.java
@@ -9,6 +9,10 @@ public class Atoi {
     public static void main(String[] args) {
         Solution solution = new Solution();
         System.out.println(solution.myAtoi("42"));
+        System.out.println(solution.myAtoi("-42"));
+        System.out.println(solution.myAtoi("4193 with words"));
+        System.out.println(solution.myAtoi("words and 987"));
+        System.out.println(solution.myAtoi("-91283472332"));
 
     }
 
@@ -17,21 +21,43 @@ public class Atoi {
             String trimmedString = s.trim();
             boolean minus = false;
             String subString = trimmedString;
+            if (trimmedString.length() == 0) {
+                return 0;
+            }
             if (trimmedString.charAt(0) == '-') {
                 minus = true;
                 subString = trimmedString.substring(1);
             } else if (trimmedString.charAt(0) == '+') {
                 subString = trimmedString.substring(1);
+            } else if (isNotNumber(trimmedString.charAt(0))) {
+                return 0;
             }
-
+            int lastIndex = subString.length();
             for (int i = 0; i < subString.length(); i++) {
                 char c = subString.charAt(i);
-                if ('0' > c  || c > '9') {
-                    System.out.println("not a number");
-
+                if (isNotNumber(c)) {
+                    lastIndex = i;
+                    break;
                 }
             }
-            return 0;
+            if (lastIndex == 0) {
+                return 0;
+            }
+            long parseLong = Long.parseLong(subString.substring(0, lastIndex));
+            if(minus){
+                parseLong = parseLong * -1;
+            }
+            if(parseLong > Integer.MAX_VALUE){
+                parseLong = Integer.MAX_VALUE;
+            }
+            if(parseLong < Integer.MIN_VALUE){
+                parseLong = Integer.MIN_VALUE;
+            }
+            return (int)parseLong;
+        }
+
+        private boolean isNotNumber(char c) {
+            return '0' > c || c > '9';
         }
     }
 }
