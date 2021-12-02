cc799dbd1653656eb0349c1c5a8c5955551e627e
harris <harrisleesh>
2021-12-02 22:24:32 +0900

    functional programming with kotlin example

---
+++ fp-with-kotlin/src/main/kotlin/ex1/OopCalculator.kt
@@ -0,0 +1,34 @@
+package ex1
+
+fun main(args: Array<String>) {
+    val plusCalculator = OopCalculator(Plus())
+    println(plusCalculator.calculate(3, 1))
+    val minusCalculator = OopCalculator(Minus())
+    println(minusCalculator.calculate(3, 1))
+}
+
+interface Calculator {
+    fun calculate(num1: Int, num2: Int): Int
+}
+
+class Plus : Calculator {
+    override fun calculate(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+}
+
+class Minus : Calculator {
+    override fun calculate(num1: Int, num2: Int): Int {
+        return num1 - num2
+    }
+}
+
+class OopCalculator (private val calculator: Calculator){
+    fun calculate(num1: Int, num2: Int): Int {
+        if (num1 > num2 && num2 != 0) {
+            return calculator.calculate(num1, num2)
+        } else{
+            throw IllegalArgumentException()
+        }
+    }
+}
