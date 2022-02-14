9fcc29b552142a8e673837fe540537f9ed24b3f3
harrisleesh <harrisleesh@gmail.com>
2022-02-15 00:13:03 +0900

    Thread Local 동시성 문제의 해결

---
+++ advanced/src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,39 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> service.logic("userA");
+
+        Runnable userB = () -> service.logic("userB");
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("threadA");
+        Thread threadB = new Thread(userB);
+        threadB.setName("threadB");
+
+        threadA.start();
+        sleep(2000);
+        threadB.start();
+
+        sleep(3000);
+        log.info("main exit");
+    }
+
+    private void sleep(int i) {
+        try {
+            Thread.sleep(i);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
