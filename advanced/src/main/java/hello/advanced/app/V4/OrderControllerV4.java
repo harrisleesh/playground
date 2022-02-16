b0f9578fb76d935812a722c68699b20f3a4fafb6
harrisleesh <harrisleesh@gmail.com>
2022-02-16 10:18:40 +0900

    Template Method pattern

---
+++ advanced/src/main/java/hello/advanced/app/V4/OrderControllerV4.java
@@ -0,0 +1,30 @@
+package hello.advanced.app.V4;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV4 {
+
+    private final OrderServiceV4 orderServiceV4;
+    private final LogTrace trace;
+
+    @GetMapping("/v4/request")
+    public String request(String itemId) {
+        AbstractTemplate<String> template = new AbstractTemplate<>(trace) {
+            @Override
+            protected String call() {
+                orderServiceV4.orderItem(itemId);
+                return "ok";
+            }
+        };
+
+        System.out.println(template.getClass());
+        return template.execute("OrderController.request()");
+    }
+}
